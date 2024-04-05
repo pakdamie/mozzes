6d5561b10bf2fe2755a28ef36fce0aa33c6edc54
Damie Pak <dkp5177@psu.edu>
2024-04-05 14:39:21 -0400

    WORKING LINEAR CHAIN TRICK CODE

--- Code/DEIP_ODE.R
+++ Code/DEIP_ODE.R
@@ -1,57 +1,123 @@
 
-### Vectorizing in progress 
 
-DEIP_ODE <- function(t,y, params){
+###PURE LINEAR CHAIN TRICK MODEL; if it's too slow I might write it in C 
+
+DEIP_ODE <- function(t, state, param) {
+       
         
- b = params['b'] #birth rate
- beta = params['beta'] # infection rate
- mu_S = params['mu_S'] #natural mortality rate of the susceptible mosquitio
- bite = params['bite'] #as of right now let's do a really simple bite rate
- alpha_E = params['alphaE'] #the development rate of the single fed exposed class
- mu_E = params['mu_E'] # the background mortality rate of the exposed class
- mu_I = params['mu_I'] #the background mortality rate of the infectious mosquito
- alpha_I = params['alphaI'] #the senescent rate 
- n_E = parmas['nE'] #the shape parameter for the exposed class
- n_D = params['nD'] #the shape parameter for the double fed class
- n_I = params['nI'] # the shape parameter for the infected class
- n_DI = params['nDI'] #the shape parameter for the double infected class 
- alpha_DI = params['alphaDI'] #the development rate of the double infected (infected)
- alpha_D = params['alphaD'] #the development rate of the double infected
- mu_D = params['muD'] #the mortality rate of the double fed
- mu_DI = params['muDI'] #the mortality rate of the double fed infected
+        S<- state[1]
+        E <- state[2:(n_E + 1)] 
+        D <- state[(n_E + 2):(n_E+n_D+1)] 
+        I <- state[(n_E+n_D +2)] 
+        DI <- state[(n_E+n_D+3)] 
+        
+        
+         with(
+                as.list(c(state, param)),
+                {
+                        
+        b = param['b'] #birth rate
+        beta = param['beta'] # infection rate
+        mu_S = param['mu_S'] #natural mortality rate of the susceptible mosquito
+        bite = param['bite'] #as of right now let's do a constant
+        alpha_E = param['alpha_E'] #the development rate of the single fed exposed class
+        mu_E = param['mu_E'] # the background mortality rate of the exposed class
+        alpha_I = param['alpha_I'] #the senescent rate 
+        n_E = param['n_E'] #the shape parameter for the exposed class
+        n_D = param['n_D'] #the shape parameter for the double fed class
+        alpha_DI = param['alpha_DI'] #the development rate of the double infected (infected)
+        alpha_D = param['alpha_D'] #the development rate of the double infected
+        mu_D = param['mu_D'] #the mortality rate of the double fed
+        VH = param['VH']
  
- dS = b - beta * S * VH - mu_S * S
  
- dE_0  = (beta * S*VH) - (n_E * alpha*E * E[1]) - (bite*E[1]) - (mu_E * E[1])
+###Susceptible mosquitos
+ dS = b - (beta * S * VH) - (mu_S * S)
  
- dE =  
- for (i in seq(2, n_E)){
+###Exposed mosquitos class
+ 
+ dE = rep(0, n_E)
+ 
+ dE[1]  <- (beta*S* VH) - (n_E * alpha_E * E[1]) - (bite*E[1]) - (mu_E * E[1])
+ 
+ if (n_E > 1) {
+         for (i in 2:n_E) {
+                 dE[i] <- n_E * alpha_E * E[i-1] - n_E * alpha_E * E[i] -
+                         (bite * E[i]) - (mu_E * E[i])
+         }
+ }
+ 
 
-         dE[n] = (n_E * alpha_E * E[n-1]) + (n_E * alpha_E * E[n]) - (bite * E[n]) - (mu_E *E[n])
-         
- }
+###Double fed exposed mosquito class
  
- dD_0 = bite *E[1] - n_D * alpha_D * D[1] - mu_D * D[1]
+dD <- rep(0, n_D)
  
- for (i in seq(2, n_D)){
-         
-         dD[n] = (n_D * alpha_D * D[n-1]) + (n_D * alpha_D * D[n]) + (bite * D[n]) - (mu_D *D[n])
- }
+dD[1] = (bite * E[1]) - n_D * alpha_D * D[1] - mu_D * D[1]
  
- dI[1] = n_E * alpha_E * E[n] - n_I * alpha_I * I[1] - mu_I * I[1]
+if (n_D > 1) {
+        for (i in 2:n_D) {
+                dD[i] <- n_D * alpha_D * D[i-1] - n_D * alpha_D * D[i] +
+                        (bite * D[i]) - (mu_D * D[i])
+        }
+}
+
+###Single- fed infected mosquito class
+
+ dI = n_E * alpha_E * E[n_E] - alpha_I * I 
  
- for (k in seq(2, n_D)){
-         dI[k] = (n_I * alpha_I * I[n-1]) + (n_I * alpha_I * I[n])  - (mu_I *I[n])
-         
-         
- }
+
+###Double-fed infected mosquito class
+ dDI = n_D * alpha_D * D[n_D] - alpha_DI * DI
  
- dDI[1] = n_D * alpha_D * D[n] - n_DI * alpha_DI * DI[1] - mu_DI * DI[1]
+    
+ return(list(c(dS, dE, dD, dI, dDI)))
+                }
+        )
+}
+
+
  
- for (k in seq(2, n_DI)){
-         dDI[k] = (n_DI * alpha_DI * DI[n-1]) + (n_DI * alpha_DI * DI[n])  - (mu_DI *DI[n])
-         
- }
- 
- }
- 
+###BAD PARAM VALUES 
+parameters_n <- c(
+        b = 0.02 , #birth rate
+        beta = 1e-3,# infection rate
+        mu_S = 1/42 , #mosquito mortality rate  
+        bite = 1e-6, #as of right now let's do a constant
+        alpha_E = 1/11,  #the development rate of the single fed exposed class
+        mu_E = 1/42, # the background mortality rate of the exposed class 
+        alpha_I = 1/42, #the senescent rate 
+        n_E = 10, #the shape parameter for the exposed class
+        n_D = 10, #the shape parameter for the double fed class
+        alpha_DI = 1/20, #the development rate of the double infected (infected)
+        alpha_D = 1/11 * (2), #the development rate of the double infected
+        mu_D = 1/20,
+        VH = 50) 
+
+inits_n <- c(S = 850, 
+             E = rep(0,parameters_n['n_E']),
+             D = rep(0,parameters_n['n_D']),
+             I  =0 ,
+             DI =0)
+
+times <- seq(0, 30, by = 1)
+
+
+out_DDE <- data.frame(ode(y = inits_n, times = times, func = DEIP_ODE ,
+               parms = parameters_n))
+
+
+
+###
+susceptible <- out_DDE[,2]
+exposed <- rowSums(out_DDE[,(3:(n_E + 2))])
+double_fed_exposed <- rowSums(out_DDE[,(n_E+3):(n_E+ n_D+2)])
+infected <- out_DDE[,(n_E + n_D +3 )]
+double_infected <- out_DDE[,(n_E + n_D +4 )]
+
+par(mfrow=c(3,2))
+plot(susceptible, type = 'l', main = 'susceptible')
+plot(exposed, type = 'l', main = 'exposed')
+plot(double_fed_exposed, type = 'l', main = 'double_fed_Exposed')
+plot(infected, type = 'l', main = 'infected')
+plot(double_infected, type = 'l', main = 'double_infected')
+
